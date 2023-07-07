b8a2c158c4f0e4838e834c5d6ece38a335dc8d01
boris <boris.lok@cienet.com>
2023-07-07 11:24:45 +0800

    [Refactoring] format the code.

--- classes/horror-types/index.test.ts
+++ classes/horror-types/index.test.ts
@@ -1,145 +1,145 @@
-import { describe, expect, it } from "@jest/globals";
+import { describe, expect, it } from '@jest/globals';
 
-import { Demon, Horror, Sorcerer } from '.'
+import { Demon, Horror, Sorcerer } from '.';
 
 class MockHorror extends Horror {
-	name = "";
-	evil: boolean;
+    name = '';
+    evil: boolean;
 
-	constructor(evil: boolean) {
-		super();
-		this.evil = evil;
-	}
+    constructor(evil: boolean) {
+        super();
+        this.evil = evil;
+    }
 
-	getPowerFrom(consumed: { power: number }) {
-		return consumed.power;
-	}
+    getPowerFrom(consumed: { power: number }) {
+        return consumed.power;
+    }
 
-	isEvil() {
-		return this.evil;
-	}
+    isEvil() {
+        return this.evil;
+    }
 }
 
 describe(Demon, () => {
-	describe("properties", () => {
-		it("only has one visible property, name", () => {
-			const demon = new Demon();
+    describe('properties', () => {
+        it('only has one visible property, name', () => {
+            const demon = new Demon();
 
-			expect(Object.keys(demon)).toEqual(["name"]);
-		});
-	});
+            expect(Object.keys(demon)).toEqual(['name']);
+        });
+    });
 
-	describe("getPower", () => {
-		it("returns half a consumed horror's power if it was evil", () => {
-			const demon = new Demon();
+    describe('getPower', () => {
+        it("returns half a consumed horror's power if it was evil", () => {
+            const demon = new Demon();
 
-			demon.doBattle(new MockHorror(true));
+            demon.doBattle(new MockHorror(true));
 
-			const mockWinner = new MockHorror(true);
-			mockWinner.doBattle(new MockHorror(true));
+            const mockWinner = new MockHorror(true);
+            mockWinner.doBattle(new MockHorror(true));
 
-			demon.doBattle(mockWinner);
+            demon.doBattle(mockWinner);
 
-			expect(demon.getPower()).toEqual(2.5);
-		});
+            expect(demon.getPower()).toEqual(2.5);
+        });
 
-		it("returns double a consumed horror's power if it was not evil", () => {
-			const demon = new Demon();
+        it("returns double a consumed horror's power if it was not evil", () => {
+            const demon = new Demon();
 
-			demon.doBattle(new MockHorror(false));
+            demon.doBattle(new MockHorror(false));
 
-			const mockWinner = new MockHorror(false);
-			mockWinner.doBattle(new MockHorror(true));
+            const mockWinner = new MockHorror(false);
+            mockWinner.doBattle(new MockHorror(true));
 
-			demon.doBattle(mockWinner);
+            demon.doBattle(mockWinner);
 
-			expect(demon.getPower()).toEqual(4);
-		});
-	});
+            expect(demon.getPower()).toEqual(4);
+        });
+    });
 
-	describe("isEvil", () => {
-		it("returns true", () => {
-			const demon = new Demon();
+    describe('isEvil', () => {
+        it('returns true', () => {
+            const demon = new Demon();
 
-			expect(demon.isEvil()).toBe(true);
-		});
-	});
+            expect(demon.isEvil()).toBe(true);
+        });
+    });
 });
 
 describe(Sorcerer, () => {
-	describe("properties", () => {
-		it("only has one visible property, name", () => {
-			const sorcerer = new Sorcerer("", true);
+    describe('properties', () => {
+        it('only has one visible property, name', () => {
+            const sorcerer = new Sorcerer('', true);
 
-			expect(Object.keys(sorcerer)).toEqual(["name"]);
-		});
-	});
+            expect(Object.keys(sorcerer)).toEqual(['name']);
+        });
+    });
 
-	describe("getPower", () => {
-		it("returns double a consumed horror's power if both are evil", () => {
-			const sorcerer = new Sorcerer("", true);
+    describe('getPower', () => {
+        it("returns double a consumed horror's power if both are evil", () => {
+            const sorcerer = new Sorcerer('', true);
 
-			sorcerer.doBattle(new MockHorror(true));
+            sorcerer.doBattle(new MockHorror(true));
 
-			const mockWinner = new MockHorror(true);
-			mockWinner.doBattle(new MockHorror(true));
+            const mockWinner = new MockHorror(true);
+            mockWinner.doBattle(new MockHorror(true));
 
-			sorcerer.doBattle(mockWinner);
+            sorcerer.doBattle(mockWinner);
 
-			expect(sorcerer.getPower()).toEqual(4);
-		});
+            expect(sorcerer.getPower()).toEqual(4);
+        });
 
-		it("returns double a consumed horror's power if neither are evil", () => {
-			const sorcerer = new Sorcerer("", false);
+        it("returns double a consumed horror's power if neither are evil", () => {
+            const sorcerer = new Sorcerer('', false);
 
-			sorcerer.doBattle(new MockHorror(false));
+            sorcerer.doBattle(new MockHorror(false));
 
-			const mockWinner = new MockHorror(false);
-			mockWinner.doBattle(new MockHorror(true));
+            const mockWinner = new MockHorror(false);
+            mockWinner.doBattle(new MockHorror(true));
 
-			sorcerer.doBattle(mockWinner);
+            sorcerer.doBattle(mockWinner);
 
-			expect(sorcerer.getPower()).toEqual(4);
-		});
+            expect(sorcerer.getPower()).toEqual(4);
+        });
 
-		it("returns half a consumed horror's power if only the sorcerer is evil", () => {
-			const sorcerer = new Sorcerer("", true);
+        it("returns half a consumed horror's power if only the sorcerer is evil", () => {
+            const sorcerer = new Sorcerer('', true);
 
-			sorcerer.doBattle(new MockHorror(false));
+            sorcerer.doBattle(new MockHorror(false));
 
-			const mockWinner = new MockHorror(false);
-			mockWinner.doBattle(new MockHorror(false));
+            const mockWinner = new MockHorror(false);
+            mockWinner.doBattle(new MockHorror(false));
 
-			sorcerer.doBattle(mockWinner);
+            sorcerer.doBattle(mockWinner);
 
-			expect(sorcerer.getPower()).toEqual(3);
-		});
+            expect(sorcerer.getPower()).toEqual(3);
+        });
 
-		it("returns half a consumed horror's power if only the consumed is evil", () => {
-			const sorcerer = new Sorcerer("", false);
+        it("returns half a consumed horror's power if only the consumed is evil", () => {
+            const sorcerer = new Sorcerer('', false);
 
-			sorcerer.doBattle(new MockHorror(false));
+            sorcerer.doBattle(new MockHorror(false));
 
-			const mockWinner = new MockHorror(true);
-			mockWinner.doBattle(new MockHorror(true));
+            const mockWinner = new MockHorror(true);
+            mockWinner.doBattle(new MockHorror(true));
 
-			sorcerer.doBattle(mockWinner);
+            sorcerer.doBattle(mockWinner);
 
-			expect(sorcerer.getPower()).toEqual(3);
-		});
-	});
+            expect(sorcerer.getPower()).toEqual(3);
+        });
+    });
 
-	describe("isEvil", () => {
-		it("returns false when the sorcerer is not evil", () => {
-			const sorcerer = new Sorcerer("", false);
+    describe('isEvil', () => {
+        it('returns false when the sorcerer is not evil', () => {
+            const sorcerer = new Sorcerer('', false);
 
-			expect(sorcerer.isEvil()).toBe(false);
-		});
+            expect(sorcerer.isEvil()).toBe(false);
+        });
 
-		it("returns true when the sorcerer is evil", () => {
-			const sorcerer = new Sorcerer("", true);
+        it('returns true when the sorcerer is evil', () => {
+            const sorcerer = new Sorcerer('', true);
 
-			expect(sorcerer.isEvil()).toBe(true);
-		});
-	});
+            expect(sorcerer.isEvil()).toBe(true);
+        });
+    });
 });
