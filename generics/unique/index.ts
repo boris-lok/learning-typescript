f61732abae280de6508f3d1ca6469febaa72139b
boris <boris.lok@cienet.com>
2023-07-10 11:37:15 +0800

    [Feature] Impl `unique` generics practice.

---
+++ generics/unique/index.ts
@@ -0,0 +1,11 @@
+export function unique<TItem>(...items: TItem[][]) {
+    const res = new Set<TItem>();
+
+    for (const row of items) {
+        for (const data of row) {
+                res.add(data);
+        }
+    }
+
+    return Array.from(res);
+}
